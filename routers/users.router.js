d29e999733855f7fd03a653e6b1e6cf2f87a8489
unknown <wogjs5656@gmail.com>
2024-01-31 16:08:00 +0900

    [Feat] login add

--- routers/users.router.js
+++ routers/users.router.js
@@ -2,6 +2,7 @@
 
 import express from 'express';
 import { prisma } from '../models/index.js';
+import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 const router = express.Router();
 
@@ -52,6 +53,30 @@ router.post('/sign-up', async (req, res, next) => {
   });
 
   return res.status(201).json({ message: '회원가입이 완료되었습니다.' });
+});
+
+/** 로그인 API **/
+
+router.post('/sign-in', async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await prisma.users.findFirst({
+    where: { email },
+  });
+  if (!user)
+    return res.status(401).json({ message: '존재하지 않는 이메일입니다.' });
+  // 입력받은 사용자의 비밀번호와 데이터베이스에 저장된 비밀번호를 비교합니다.
+  else if (!(await bcrypt.compare(password, user.password)))
+    return res.status(401).json({ message: '비밀번호가 일치하지 않습니다.' });
+
+  const token = jwt.sign(
+    {
+      userId: user.userId,
+    },
+    'custom-secret-key'
+  );
+
+  res.cookie('authorization', `Bearer ${token}`);
+  return res.status(201).json({message: '로그인 성공'})
 });
 
 export default router;
