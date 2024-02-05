7ccb138ff892a0e8027daebac64869f0bda98032
unknown <wogjs5656@gmail.com>
2024-02-05 20:34:57 +0900

    [Feat] 자동 로그인 api추가

--- routers/users.router.js
+++ routers/users.router.js
@@ -9,51 +9,95 @@ const router = express.Router();
 
 /** 사용자 회원가입 API **/
 router.post('/sign-up', async (req, res, next) => {
-  const { email, password, passwordRe, name, age, gender } = req.body;
-  const isExistUser = await prisma.users.findFirst({
-    where: {
-      email,
-    },
-  });
+  const { email, clientId, password, passwordRe, name, age, gender } = req.body;
 
-  if (isExistUser) {
-    return res.status(400).json({ message: '이미 존재하는 이메일입니다.' });
+  if (!clientId) {
+    if (!email) {
+      return res.status(400).json({ message: '이메일은 필수 값 입니다.' });
+    }
+    if (!password) {
+      return res.status(400).json({ message: '이메일은 필수 값 입니다.' });
+    }
+    if (!passwordRe) {
+      return res.status(400).json({ message: '이메일은 필수 값 입니다.' });
+    }
+    if (password.length < 6) {
+      return res.status(401).json({
+        message: '입력하신 비밀번호가 6자 미만입니다',
+      });
+    }
+    if (password !== passwordRe) {
+      return res.status(401).json({
+        message: '입력하신 비밀번호가 일치하지 않습니다',
+      });
+    }
+  
   }
 
-  if (password.length < 6) {
-    return res.status(401).json({
-      message: '입력하신 비밀번호가 6자 미만입니다',
+  // clientId kakao 로그인
+  if (clientId) {
+    const isExistUser = await prisma.users.findFirst({
+      where: {
+        clientId,
+      },
     });
-  }
 
-  if (password !== passwordRe) {
-    return res.status(401).json({
-      message: '입력하신 비밀번호가 일치하지 않습니다',
+    if (isExistUser) {
+      return res.status(400).json({ message: '이미 가입된 사용자 입니다.' });
+    }
+
+    // Users 테이블에 사용자를 추가합니다.
+    const user = await prisma.users.create({
+      data: {
+        clientId,
+      },
     });
+    // UserInfos 테이블에 사용자 정보를 추가합니다.
+    const userInfo = await prisma.userInfos.create({
+      data: {
+        userId: user.userId, // 생성한 유저의 userId를 바탕으로 사용자 정보를 생성합니다.
+        name,
+        age,
+        gender: gender.toUpperCase(), // 성별을 대문자로 변환합니다.
+      },
+    });
+
+    return res.status(201).json({ message: '회원가입이 완료되었습니다.' });
+  } else {
+    // email 검증
+    const isExistUser = await prisma.users.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (isExistUser) {
+      return res.status(400).json({ message: '이미 존재하는 이메일입니다.' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+  
+    // Users 테이블에 사용자를 추가합니다.
+    const user = await prisma.users.create({
+      data: {
+        email,
+        password: hashedPassword,
+        passwordRe: password,
+      },
+    });
+  
+    // UserInfos 테이블에 사용자 정보를 추가합니다.
+    const userInfo = await prisma.userInfos.create({
+      data: {
+        userId: user.userId, // 생성한 유저의 userId를 바탕으로 사용자 정보를 생성합니다.
+        name,
+        age,
+        gender: gender.toUpperCase(), // 성별을 대문자로 변환합니다.
+      },
+    });
+  
+    return res.status(201).json({ message: '회원가입이 완료되었습니다.' });
   }
-
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  // Users 테이블에 사용자를 추가합니다.
-  const user = await prisma.users.create({
-    data: {
-      email,
-      password: hashedPassword,
-      passwordRe: password,
-    },
-  });
-
-  // UserInfos 테이블에 사용자 정보를 추가합니다.
-  const userInfo = await prisma.userInfos.create({
-    data: {
-      userId: user.userId, // 생성한 유저의 userId를 바탕으로 사용자 정보를 생성합니다.
-      name,
-      age,
-      gender: gender.toUpperCase(), // 성별을 대문자로 변환합니다.
-    },
-  });
-
-  return res.status(201).json({ message: '회원가입이 완료되었습니다.' });
 });
 
 /** 로그인 API **/
@@ -69,14 +113,14 @@ router.post('/sign-in', async (req, res, next) => {
   else if (!(await bcrypt.compare(password, user.password)))
     return res.status(401).json({ message: '비밀번호가 일치하지 않습니다.' });
 
-    // 토큰의 키 값을 need-signin.middleware에 전달 하려고 custiom-secret-key 사용
+  // 토큰의 키 값을 need-signin.middleware에 전달 하려고 custiom-secret-key 사용
   const token = jwt.sign(
     {
       userId: user.userId,
     },
     'custom-secret-key',
     {
-      expiresIn: '12h'
+      expiresIn: '12h',
     }
   );
 
