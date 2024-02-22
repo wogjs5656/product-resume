4d34680eb8a857c219c64803a882f4a660785f2d
wogjs5656 <wogjs5656@gmail.com>
2024-02-22 15:32:26 +0900

    [Fix] 예외처리 추가 및 메시지 변경

--- src/controllers/resume.controller.js
+++ src/controllers/resume.controller.js
@@ -66,11 +66,15 @@ export class ResumeController {
       if (!resumeId) {
         return res.status(400).json({ message: 'resumeId는 필수 값입니다.' });
       }
+
       const resume = await this.resumeService.findDeResume(resumeId);
 
+      if(!resume) {
+        return res.json({data:resume})
+      }
       return res
         .status(200)
-        .json({ message: '상품 상세 정보를 조회하였습니다.', data: resume });
+        .json({ message: '상세 정보를 조회하였습니다.', data: resume });
     } catch (err) {
       next(err);
     }
